594383cfefa8f03f4f7ec9b82d7e7e1a7f299b72
YannyOuzid <55101971+YannyOuzid@users.noreply.github.com>
2020-11-15 01:08:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//Question 3
+let planets = ['earth', 'mars', 'jupiter', 'saturn', 'neptune']
+
+//Question 4
+console.log(planets)
+planets.forEach(element => console.log(element));
+
+//Question 7
+var hello = function (){
+
+    return 'Hello World'
+}
+
+//Question 8
+console.log(hello())
+
